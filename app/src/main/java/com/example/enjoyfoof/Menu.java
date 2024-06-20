5aa5aa88d11e1c35adedc6fcecda883373974e32
JEAN-PAUL MUKAD <21km179@esisalama.org>
2024-06-20 21:28:56 +0200

    Adaptation des textes par rapport aux ecrans

--- app/src/main/java/com/example/enjoyfoof/Menu.java
+++ app/src/main/java/com/example/enjoyfoof/Menu.java
@@ -1,9 +1,6 @@
 package com.example.enjoyfoof;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,28 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class Menu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-            this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_menu);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-    }
-    public void register(View view){
-        startActivity(new Intent(WelcomeActivity.this, RegistrationActivity.class));
-
-    }
-
-    public void Login(View view) {
-        startActivity(new Intent(WelcomeActivity.this, RegistrationActivity.class));
-
     }
 }
