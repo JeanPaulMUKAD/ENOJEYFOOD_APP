43513295e8660ef593161ea4513b0c82ac1b9e1c
JEAN-PAUL MUKAD <21km179@esisalama.org>
2024-06-20 12:26:57 +0200

    Debut de l'interface de connexion

--- app/src/main/java/com/example/enjoyfoof/WelcomeActivity.java
+++ app/src/main/java/com/example/enjoyfoof/WelcomeActivity.java
@@ -1,6 +1,9 @@
 package com.example.enjoyfoof;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,13 @@ public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+            this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_welcome);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
+    public void register(View view){
+        startActivity(new Intent(WelcomeActivity.this, RegistrationActivity.class));
+
+    }
+
 }
