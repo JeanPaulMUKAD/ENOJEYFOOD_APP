5ddbc491a0ba58b5a7e267978eff3a56413708eb
JEAN-PAUL MUKAD <21km179@esisalama.org>
2024-06-24 22:47:13 +0200

    Ajout des codes

--- app/src/main/java/com/example/enjoyfoof/adapters/HomeVerAdapter.java
+++ app/src/main/java/com/example/enjoyfoof/adapters/HomeVerAdapter.java
@@ -1,6 +1,43 @@
 package com.example.enjoyfoof.adapters;
 
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class HomeVerAdapter extends RecyclerView.Adapter<> {
+import com.example.enjoyfoof.models.HomeVerModel;
+
+import java.util.List;
+
+public class HomeVerAdapter extends RecyclerView.Adapter<HomeVerAdapter.ViewHolder>{
+
+    Context context;
+    List<HomeVerModel>list;
+    public HomeVerAdapter(Context context, List<HomeVerModel> list){
+        this.context = context;
+        this.list = list;
+    }
+   @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
