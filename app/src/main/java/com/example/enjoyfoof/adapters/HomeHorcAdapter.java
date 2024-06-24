38c633195d5a02966028701641de04d881a34ef8
JEAN-PAUL MUKAD <21km179@esisalama.org>
2024-06-24 11:01:41 +0200

    Ajout des codes

---
+++ app/src/main/java/com/example/enjoyfoof/adapters/HomeHorcAdapter.java
@@ -0,0 +1,28 @@
+package com.example.enjoyfoof.adapters;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HomeHorcAdapter extends RecyclerView.Adapter<HomeHorcAdapter.viewHolder> {
+
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class viewHolder {
+    }
+}
---
+++ app/src/main/java/com/example/enjoyfoof/models/HomeHorcModel.java
@@ -0,0 +1,26 @@
+package com.example.enjoyfoof.models;
+
+public class HomeHorcModel{
+    int image;
+    String name;
+
+    public HomeHorcModel(int image, String name){
+        this.image = image;
+        this.name = name;
+    }
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
