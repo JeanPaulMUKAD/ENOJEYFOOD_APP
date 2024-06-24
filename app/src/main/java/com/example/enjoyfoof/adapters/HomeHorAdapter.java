9b394afccb4e24a800daa4a855e90073050b635e
JEAN-PAUL MUKAD <21km179@esisalama.org>
2024-06-24 21:43:37 +0200

    Ajout des codes

--- app/src/main/java/com/example/enjoyfoof/adapters/HomeHorAdapter.java
+++ app/src/main/java/com/example/enjoyfoof/adapters/HomeHorAdapter.java
@@ -1,4 +1,33 @@
 package com.example.enjoyfoof.adapters;
 
-public class HomeHorAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HomeHorAdapter extends RecyclerView.Adapter<HomeHorAdapter.ViweHolder> {
+
+    @NonNull
+    @Override
+    public ViweHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViweHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViweHolder extends RecyclerView.ViewHolder {
+
+        public ViweHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
