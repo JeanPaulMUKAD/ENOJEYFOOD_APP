8f19fb3d6b26af91d57e3ac4712094870dcd9ccd
JEAN-PAUL MUKAD <21km179@esisalama.org>
2024-06-24 16:18:05 +0200

    Ajout des codes

--- app/src/main/java/com/example/enjoyfoof/adapters/UpdateVerticalRec.java
+++ app/src/main/java/com/example/enjoyfoof/adapters/UpdateVerticalRec.java
@@ -5,5 +5,5 @@ import com.example.enjoyfoof.models.HomeHorcModel;
 import java.util.ArrayList;
 
 public interface UpdateVerticalRec {
-    public class callBack(int position, ArrayList<HomeVerAdapter> list)
+
 }
