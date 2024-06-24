a354c55baafcd8a5ce16e67dacd390ec13796218
JEAN-PAUL MUKAD <21km179@esisalama.org>
2024-06-24 21:49:07 +0200

    Ajout des codes

--- app/src/main/java/com/example/enjoyfoof/adapters/HomeHorAdapter.java
+++ app/src/main/java/com/example/enjoyfoof/adapters/HomeHorAdapter.java
@@ -1,12 +1,23 @@
 package com.example.enjoyfoof.adapters;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.enjoyfoof.R;
+import com.example.enjoyfoof.models.HomeHorModel;
+
+import java.util.List;
+
 public class HomeHorAdapter extends RecyclerView.Adapter<HomeHorAdapter.ViweHolder> {
+
+    Context context;
+    List<HomeHorModel> list;
 
     @NonNull
     @Override
@@ -25,9 +36,13 @@ public class HomeHorAdapter extends RecyclerView.Adapter<HomeHorAdapter.ViweHold
     }
 
     public class ViweHolder extends RecyclerView.ViewHolder {
-
+        ImageView imageView;
+        TextView name;
         public ViweHolder(@NonNull View itemView) {
             super(itemView);
+
+            imageView = itemView.findViewById(R.id.hor_image);
+            name = itemView.findViewById(R.id.hor_text);
         }
     }
 }
