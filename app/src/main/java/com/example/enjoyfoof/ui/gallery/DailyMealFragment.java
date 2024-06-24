39828ccf9c7f5debb091d98f4dd8e03e2974f169
JEAN-PAUL MUKAD <21km179@esisalama.org>
2024-06-24 09:41:08 +0200

    code de fragment home

--- app/src/main/java/com/example/enjoyfoof/ui/gallery/DailyMealFragment.java
+++ app/src/main/java/com/example/enjoyfoof/ui/gallery/DailyMealFragment.java
@@ -1,25 +1,23 @@
-package com.example.enjoyfoof.ui.slideshow;
+package com.example.enjoyfoof.ui.gallery;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
-import com.example.enjoyfoof.databinding.FragmentSlideshowBinding;
+import com.example.enjoyfoof.databinding.FragmentGalleryBinding;
 
-public class SlideshowFragment extends Fragment {
+public class DailyMealFragment extends Fragment {
 
-    private FragmentSlideshowBinding binding;
+    private FragmentGalleryBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
+        binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         return root;
--- app/src/main/java/com/example/enjoyfoof/ui/slideshow/FavouriteFragment.java
+++ app/src/main/java/com/example/enjoyfoof/ui/slideshow/FavouriteFragment.java
@@ -1,25 +1,24 @@
-package com.example.enjoyfoof.ui.gallery;
+package com.example.enjoyfoof.ui.slideshow;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
-import com.example.enjoyfoof.R;
-import com.example.enjoyfoof.databinding.FragmentGalleryBinding;
+import com.example.enjoyfoof.databinding.FragmentSlideshowBinding;
 
-public class GalleryFragment extends Fragment {
+public class FavouriteFragment extends Fragment {
 
-    private FragmentGalleryBinding binding;
+    private FragmentSlideshowBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+
+        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
         return root;
     }
