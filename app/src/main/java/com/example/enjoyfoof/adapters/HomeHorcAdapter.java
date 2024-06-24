d5ca91c0c03aa05a641d53c8fe297f7937b4ba3b
JEAN-PAUL MUKAD <21km179@esisalama.org>
2024-06-24 12:04:41 +0200

    Ajout des codes

--- app/src/main/java/com/example/enjoyfoof/adapters/HomeHorcAdapter.java
+++ app/src/main/java/com/example/enjoyfoof/adapters/HomeHorcAdapter.java
@@ -1,28 +1,55 @@
 package com.example.enjoyfoof.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.enjoyfoof.R;
+import com.example.enjoyfoof.models.HomeHorcModel;
+
+import java.util.List;
+
 public class HomeHorcAdapter extends RecyclerView.Adapter<HomeHorcAdapter.viewHolder> {
+
+    Context context;
+    List<HomeHorcModel> list;
+    public HomeHorcAdapter(Context context, List<HomeHorcModel> list){
+        this.context = context;
+        this.list = list;
+    }
 
     @NonNull
     @Override
     public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new viewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.home_horizontal_item, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull viewHolder holder, int position) {
-
+        holder.imageView.setImageResource(list.get(position).getImage());
+        holder.name.setText(list.get(position).getName());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
     }
 
-    public class viewHolder {
+    public class viewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        TextView name;
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.hor_image);
+            name = itemView.findViewById(R.id.hor_text);
+        }
     }
 }
--- app/src/main/java/com/example/enjoyfoof/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/enjoyfoof/ui/home/HomeFragment.java
@@ -9,11 +9,22 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.enjoyfoof.R;
+import com.example.enjoyfoof.adapters.HomeHorcAdapter;
 import com.example.enjoyfoof.databinding.FragmentHomeBinding;
+import com.example.enjoyfoof.models.HomeHorcModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    RecyclerView homeHorizontalRec;
+    List<HomeHorcModel> homeHorcModelList;
+    HomeHorcAdapter homeHorcAdapter;
     private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -22,7 +33,19 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        homeHorizontalRec = root.findViewById(R.id.home_hor_rec);
+        homeHorcModelList = new ArrayList<>();
+        homeHorcModelList.add(new HomeHorcModel(R.drawable.pizza, "Pizza"));
+        homeHorcModelList.add(new HomeHorcModel(R.drawable.humberger, "Humberger"));
+        homeHorcModelList.add(new HomeHorcModel(R.drawable.fries, "Fries"));
+        homeHorcModelList.add(new HomeHorcModel(R.drawable.cream, "Ice Cream"));
+        homeHorcModelList.add(new HomeHorcModel(R.drawable.sandwich, "Sandwich"));
 
+        homeHorcAdapter = new HomeHorcAdapter(getActivity(),homeHorcModelList);
+        homeHorizontalRec.setAdapter(homeHorcAdapter);
+        homeHorizontalRec.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
+        homeHorizontalRec.setHasFixedSize(true);
+        homeHorizontalRec.setNestedScrollingEnabled(false);
         return root;
     }
 
