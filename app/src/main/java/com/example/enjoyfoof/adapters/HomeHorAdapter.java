addc04b55e432f4e4e35e752360c6d75ecc692cf
JEAN-PAUL MUKAD <21km179@esisalama.org>
2024-06-24 22:00:42 +0200

    Ajout des codes

--- app/src/main/java/com/example/enjoyfoof/adapters/HomeHorAdapter.java
+++ app/src/main/java/com/example/enjoyfoof/adapters/HomeHorAdapter.java
@@ -1,6 +1,7 @@
 package com.example.enjoyfoof.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -18,21 +19,27 @@ public class HomeHorAdapter extends RecyclerView.Adapter<HomeHorAdapter.ViweHold
 
     Context context;
     List<HomeHorModel> list;
+    public HomeHorAdapter(Context context, List<HomeHorModel> list){
+        this.context = context;
+        this.list = list;
+    }
 
     @NonNull
     @Override
     public ViweHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new ViweHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.home_horizontal_item, parent,false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViweHolder holder, int position) {
+         holder.imageView.setImageResource(list.get(position).getImage());
+         holder.name.setText(list.get(position).getName());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
     }
 
     public class ViweHolder extends RecyclerView.ViewHolder {
--- app/src/main/java/com/example/enjoyfoof/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/enjoyfoof/ui/home/HomeFragment.java
@@ -1,4 +1,19 @@
 package com.example.enjoyfoof.ui.home;
 
-public class HomeFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.enjoyfoof.R;
+
+public class HomeFragment extends Fragment {
+    public View onCreatView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        return  root;
+    }
+
 }
