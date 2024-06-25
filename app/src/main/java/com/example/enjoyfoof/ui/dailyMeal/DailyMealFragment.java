56b608f318c2716eb299cde657952e94cbbef13d
JEAN-PAUL MUKAD <21km179@esisalama.org>
2024-06-25 04:08:04 +0200

    Ajout des codes

--- app/src/main/java/com/example/enjoyfoof/ui/dailyMeal/DailyMealFragment.java
+++ app/src/main/java/com/example/enjoyfoof/ui/dailyMeal/DailyMealFragment.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.enjoyfoof.R;
@@ -28,14 +29,18 @@ public class DailyMealFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.daily_meal_fragment, container, false);
-
         recyclerView = root.findViewById(R.id.daily_meal_rec);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         dailyMealModels = new ArrayList<>();
         dailyMealModels.add(new DailyMealModel(R.drawable.breakfast, "BreakFast", "30% OFF", "Description"));
         dailyMealModels.add(new DailyMealModel(R.drawable.cofee, "Coffee", "30% OFF", "Description"));
-        dailyMealModels.add(new DailyMealModel(R.drawable.lunch, "Lunch", "30% OFF", "Description"));
-        dailyMealModels.add(new DailyMealModel(R.drawable.sweet, "Sweet", "30% OFF", "Description"));
-        dailyMealModels.add(new DailyMealModel(R.drawable.dinner, "Dinner", "30% OFF", "Description"));
+        dailyMealModels.add(new DailyMealModel(R.drawable.lunch, "Lunch", "20% OFF", "Description"));
+        dailyMealModels.add(new DailyMealModel(R.drawable.sweet, "Sweet", "50% OFF", "Description"));
+        dailyMealModels.add(new DailyMealModel(R.drawable.dinner, "Dinner", "25% OFF", "Description"));
+
+        dailyMealAdapter = new DailyMealAdapter(getContext(),dailyMealModels);
+        recyclerView.setAdapter(dailyMealAdapter);
+        dailyMealAdapter.notifyDataSetChanged();
 
         return root;
     }
