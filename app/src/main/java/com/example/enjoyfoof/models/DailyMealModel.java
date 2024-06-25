9e58942fa8744ea7d63ac384e856de28e87701ee
JEAN-PAUL MUKAD <21km179@esisalama.org>
2024-06-25 03:03:48 +0200

    Ajout des codes

--- app/src/main/java/com/example/enjoyfoof/models/DailyMealModel.java
+++ app/src/main/java/com/example/enjoyfoof/models/DailyMealModel.java
@@ -1,4 +1,60 @@
 package com.example.enjoyfoof.models;
 
 public class DailyMealModel {
+    int image;
+    String name;
+    String discount;
+    String type;
+    String description;
+
+    // Constructeur
+    public DailyMealModel(int image, String name, String discount, String type, String description) {
+        this.image = image;
+        this.name = name;
+        this.discount = discount;
+        this.type = type;
+        this.description = description;
+    }
+
+    // Getters
+    public int getImage() {
+        return image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDiscount() {
+        return discount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    // Setters
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDiscount(String discount) {
+        this.discount = discount;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
